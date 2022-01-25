744e8072efc51c61ee139a1ad064d724e940b514
latermonk <iback.china@gmail.com>
2022-01-25 10:01:22 +0800

    add gf-demo project

---
+++ 20220125/gf-demos/go.mod
@@ -0,0 +1,5 @@
+module github.com/gogf/gf-demos/v2
+
+go 1.15
+
+require github.com/gogf/gf/v2 v2.0.0-rc.0.20220122074538-e517bf7b0e8d
---
+++ 20220125/gf-demos/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gogf/gf-demos/v2/internal/cmd"
+	"github.com/gogf/gf/v2/os/gctx"
+)
+
+func main() {
+	cmd.Main.Run(gctx.New())
+}
